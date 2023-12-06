697e6d4f87c6812ce6bf5b51dbfe14cf50adf674
Morsed <mdmorsedalam324@gmail.com>
2023-12-06 19:44:25 +0600

    Some changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://my-eleven-assignment.web.app"],
+    origin: ["http://localhost:5173", "https://my-eleven-assignment.web.app","https://career-maker-web.netlify.app"],
     credentials: true,
   })
 );
